7a312db1da415df16c2f7096132a228425dc02d8
Sammy <13516103@std.stei.itb.ac.id>
2018-11-26 04:26:34 +0000

    Finished webapp

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 
 clf = hdc()
 
+def execute_classify(features):
+    features_nanified = [x if x is not None else NAN for x in features]
+    output_i64, = clf.predict(features_nanified)
+    output = int(output_i64)
+    if output == 0:
+        label = 'Absent'
+    else:
+        label = 'Present ({})'.format(output)
+    return output, label
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,11 +28,14 @@ attributes = ['age', 'sex', 'chestPain', 'bloodPressure', 'serumCholesterol',
 @app.route('/check', methods=['POST'])
 def check():
     global clf
+    import time
+    time.sleep(2)
     features = []
     for attr in attributes:
-        features.append(request.form.get(attr, default=NAN, type=float))
-    output_i64, = clf.predict(features)
-    output = int(output_i64)
-    features_denanified = [x if x is not NAN else None for x in features]
-    features_pretty = {x: y for (x, y) in zip(attributes, features_denanified)}
-    return jsonify({"features": features_pretty, "output": output})
+        features.append(request.form.get(attr, default=None, type=float))
+    output, label = execute_classify(features)
+    features_pretty = {x: y for (x, y) in zip(attributes, features)}
+    if request.accept_mimetypes.accept_html:
+        return render_template('result.html', result=label)
+    else:
+        return jsonify({"features": features_pretty, "output": output, "label": label})
